3b7a1f52b92f6176dc9f8787f8cd5907e88a211b
JennyFung27 <150959610+JennyFung27@users.noreply.github.com>
2026-07-12 12:31:59 -0700

    Add cumulative sales analysis query
    Add cumulative analysis SQL to calculate monthly and running total sales.

--- scripts/02_cumulative_analysis.sql
+++ scripts/02_cumulative_analysis.sql
@@ -1,1 +1,15 @@
+/*Cumulative Analysis--calculate the total sales per month 
+and the running total of sales over time*/
+SELECT
+    order_date,
+    total_sales,
+    sum(total_sales) over (ORDER BY order_date) running_total_sales
+FROM (
 
+SELECT
+    DATETRUNC(month, order_date) order_date,
+    SUM(sales_amount) total_sales
+FROM gold.fact_sales
+WHERE order_date is NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+) t
